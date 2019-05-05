279df91fc2ee422dcde5020119f284d410c678a9
TH <thebinarymonster@outlook.com>
2019-05-05 16:06:20 -0700

    add file

---
+++ routine_chooser.py
@@ -0,0 +1,12 @@
+import random
+certified_moves = [['straight jump',1], ['tuck jump',1], ['straddle jump',1], ['pike jump',1], ['half turn',1],['full turn',3], ['seat drop',1], ['box drop',2], ['front drop',2], ['back drop',5], ['three quarter front with hands',4], ['three quarter front without hands',5], ['cartwheel',4], ['roundoff',4]];
+
+for j in range(20):
+
+    for i in range(7):
+        print(random.choice(certified_moves)
+
+    print('');
+#difficulty points: straight jump-1, tuck jump-1, straddle jump-1, pike jump-1, half turn-1, full turn-3, seat drop-1, 
+#box drop-2, front drop-2, back drop-5, three quarterfront with hands-4, three quarter front without hands-5, cartwheel-4,
+#roundoff-4
